feed667c36203274c2b8f0d4499c8beff4f54c47
hrishi-19 <hrishakamath.com>
2021-12-08 19:08:12 +0530

    added timer function

--- lib/screens/pomodoro.dart
+++ lib/screens/pomodoro.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class Pomodoro extends StatefulWidget {
   const Pomodoro({Key? key}) : super(key: key);
@@ -6,9 +7,78 @@ class Pomodoro extends StatefulWidget {
   _PomodoroState createState() => _PomodoroState();
 }
 
-class _PomodoroState extends State<Pomodoro> {
+class _PomodoroState extends State<Pomodoro>with TickerProviderStateMixin {
+  late AnimationController controller;
+  bool playing=false;
+
+  @override
+  void initState() {
+    super.initState();
+    controller=AnimationController(vsync: this,duration: Duration(minutes: 25));
+  }
+  String get CountText{
+    Duration count=controller.duration! * controller.value;
+    return "${(count.inMinutes %60).toString().padLeft(2,'0')}:${(count.inSeconds %60).toString().padLeft(2,'0')}";
+  }
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xfff5fbff),
+      body: Column(
+        children: [
+          Expanded(
+              child: Center(
+                child: AnimatedBuilder(
+                  animation: controller,
+                  builder: (context,child)=> Text(CountText,
+                  style: TextStyle(
+                    fontSize: 55,
+                    fontWeight: FontWeight.bold
+                  ),),
+                ),
+              )),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(onPressed: (){
+                  if(controller.isAnimating){
+                    controller.stop();
+                    setState(() {
+                      playing=false;
+                    });
+                  }else{
+                    controller.reverse(from:controller.value==0?1.0:controller.value);
+                    setState(() {
+                      playing=true;
+                    });
+                  }
+
+                },
+                child:Icon(
+                  Icons.play_arrow_rounded
+
+                )),
+                SizedBox(width: 30,),
+                FloatingActionButton(onPressed: (){
+                  controller.reset();
+                  },
+                    child:Icon(
+                        Icons.reset_tv
+                    )
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
