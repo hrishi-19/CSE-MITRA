e781dcc91ff67b5e464f45c05aa9565871555cef
hrishi-19 <hrishakamath.com>
2021-12-08 17:52:32 +0530

    adding class

---
+++ lib/screens/notes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Notes extends StatelessWidget {
+  const Notes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
