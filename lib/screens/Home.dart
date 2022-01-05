9ba3f778f976d29b9bd4eebdb1e15f4e4a938261
hrishi-19 <hrishakamath.com>
2022-01-05 12:03:36 +0530

    adding assignments layout

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mitra/provider/authentication.dart';
-import 'package:mitra/screens/tasks.dart';
 import 'package:mitra/widgets/HomeWidget.dart';
 
 class Home extends StatefulWidget {
@@ -13,36 +12,30 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   AuthClass authClass=AuthClass();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        backgroundColor: Colors.grey[100],
+        backgroundColor: Colors.white,
         body:SafeArea(
           child: Padding(
-            padding: EdgeInsets.all(15),
+            padding: EdgeInsets.all(5),
             child: Column(
               children: [
                 Expanded(child: ListView(
                   children: [
-                    Spacer(),
-                    HomeWidget(heading: 'Tasks', description: 'Complete your tasks with \n your the help of your mitra! ', route: '/task',img_path: 'assets/images/todo.jpg',),
-                    Spacer(),
                     HomeWidget(heading:'Pomodoro', description: 'Be more productive with\n our work!',route: '/pomodoro',img_path: 'assets/images/pomodoro-timer.jpg'),
-                    Spacer(),
-                    HomeWidget(heading: 'Notes', description: 'checkout the prescribed \n textbooks',route: '/notes',img_path: 'assets/images/book.png')
+                    HomeWidget(heading: 'Assignments', description: 'upload your \n assignmenst here',route: '/notes',img_path: 'assets/images/book.png'),
+                    RaisedButton(
+                      child: Text("logout"),
+                      onPressed: (){authClass.signout(context);
+
+                      },
+                    ),
+
                   ],
                 )),
-                Positioned(
-                    top: 0,right: 0,
-                    child: RaisedButton(onPressed: (){
-                      authClass.signout(context);
 
-                    },color: Colors.red,
-                      child: Text("sign out",
-                      style: TextStyle(
-                        color: Colors.white
-                      ),),)),
 
               ],
 
--- lib/widgets/HomeWidget.dart
+++ lib/widgets/HomeWidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mitra/screens/notes.dart';
 class HomeWidget extends StatelessWidget {
   final String heading;
@@ -12,60 +13,82 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder:(context,constraints)=>Container(
-          width:constraints.maxWidth,
-          padding: EdgeInsets.symmetric(horizontal: 20,vertical: 30),
-          margin: EdgeInsets.only(
-              bottom: 25
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.white,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [Text(heading,
-                  style: TextStyle(
-                      color: Color(0XFF211551),
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold
-                  ),),
-                  Padding(padding: EdgeInsets.only(
-                      top: 20
-                  )),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text(description,
+      builder:(context,constraints)=>Padding(
+        padding: EdgeInsets.all(20),
+        child: Container(
+            width:constraints.maxWidth,
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+            margin: EdgeInsets.only(
+                bottom: 25
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  offset: Offset(4.0,4.0),
+                  spreadRadius: 1.0,
+                  blurRadius: 10.0),
+                BoxShadow(
+                    color: Colors.grey.shade50,
+                    offset: Offset(-4.0,-4.0),
+                    spreadRadius: 1.0,
+                    blurRadius: 10.0),
+              ]
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [Text(heading,
                       style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          color: Color(0XFF868290),
-                          fontSize: 16
-                      ),),
-                  ),
-                  RaisedButton(onPressed: (){
-                    Navigator.pushNamed(context, route);
-                  },color: Colors.green,
-                    child: Text("GO",
-                      style: TextStyle(
-                          fontSize: 20,
-                          color:Colors.white,
+                          color: Color(0XFF211551),
+                          fontSize: 22,
                           fontWeight: FontWeight.bold
-                      ),),)
+                      ),),
+                      Padding(padding: EdgeInsets.only(
+                          top: 20
+                      )),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: Text(description,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Color(0XFF868290),
+                              fontSize: 16,
 
-                ],
-              ),
-              Container(
-                width: 60,
-                height: 60,
-                child: Image(
-                  image: AssetImage(img_path),
+                          ),),
+                      ),
+                      RaisedButton(onPressed: (){
+                        Navigator.pushNamed(context, route);
+                      },color: Colors.green,
+                        child: Text("GO",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color:Colors.white,
+                              fontWeight: FontWeight.bold
+                          ),),)
+
+                    ],
+                  ),
                 ),
-              )
-            ],
-          ),
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    child: Image(
+                      image: AssetImage(img_path),
+                    ),
+                  ),
+                )
+              ],
+            ),
+        ),
       ),
     );
   }
