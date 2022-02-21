4d787fab53617dc5bc4fb3421da910e3e60210cd
hrishi-19 <hrishakamath.com>
2022-02-21 22:49:52 +0530

    replace listview with listview builder

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,8 @@ import 'package:google_fonts/google_fonts.dart';
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
