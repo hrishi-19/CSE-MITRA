45b9b567a85b891f146db677afcb543a1058b6e8
hrishi-19 <hrishakamath.com>
2022-03-25 22:28:34 +0530

    horizontal implementaion of listview

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -20,14 +20,14 @@ class _HomeState extends State<Home> {
         desc: 'upload your\n assignments here',
         route:'/notes',
         img_url:'assets/images/book.png'  ),
-    homecard(title: 'Attendence',
-        desc: 'check your\n attendence',
+    homecard(title: 'Results',
+        desc: 'check your\n results',
         route:'/attendence',
-        img_url:'assets/images/attd.jpg'  ),
+        img_url:'assets/images/attd.png'  ),
     homecard(title: 'Pomodoro',
         desc: 'Be more productive with\n our work!',
         route:'/pomodoro',
-        img_url:'assets/images/pomodoro-timer.jpg'  ),
+        img_url:'assets/images/pomodoro-timer.png'  ),
 
   ];
 
@@ -57,28 +57,47 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.white,
         body:SafeArea(
             child: Container(
+              height: MediaQuery.of(context).size.height,
+              padding: EdgeInsets.symmetric(vertical: 5,horizontal: 5),
               decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/bg.jpg"),
-                    fit: BoxFit.cover
-                ),
+                // image: DecorationImage(
+                //     image: AssetImage("assets/images/bg.jpg"),
+                //     fit: BoxFit.cover
+                // ),
+                color:  Color(0xFFF3E5F5),
+
               ),
-              child: Column(
-                children: [
-                  Expanded(
-                      child:ListView.builder(
-                        itemCount: widgets.length,
-                          itemBuilder: (context,index){
-                          return widgets[index];
-                          }
-                      ) ),
-                  RaisedButton(onPressed: (){
-                    authClass.signout(context);
-                  },child: Text("logout"),)
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 250,
+
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 30,),
+                    Container(
+                      height: 300,
+                        child:ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: widgets.length,
+                            itemBuilder: (context,index){
+                            return widgets[index];
+
+                            }
+                        ) ),
+                    RaisedButton(onPressed: (){
+                      authClass.signout(context);
+                    },child: Text("logout"),)
 
 
-                ],
+                  ],
 
+                ),
               ),
             ),
         ),
